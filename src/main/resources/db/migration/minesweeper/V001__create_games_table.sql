31dfc2199ef112e8d58d4aaff3051c836c62a9ee
Gaston Tulipani <gtulipani@hotmail.com>
2019-11-30 13:49:57 -0300

    Removed GameConfiguration and included inside Game directly

--- src/main/resources/db/migration/minesweeper/V001__create_games_table.sql
+++ src/main/resources/db/migration/minesweeper/V001__create_games_table.sql
@@ -2,6 +2,8 @@ CREATE TABLE `games` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `game_configuration_id` bigint(11) NOT NULL,
+  `rows_quantity` bigint(11) unsigned NOT NULL,
+  `columns_quantity` bigint(11) unsigned NOT NULL,
+  `mines_quantity` bigint(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
